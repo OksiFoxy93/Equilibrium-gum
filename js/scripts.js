7661a966f3f413580b6d22afeb41dc90b3681312
Oksana <ksibash93@gmail.com>
2021-04-07 17:56:57 +0300

    menu fadeIn, fadeOut

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,16 @@ document.querySelector('.c-hamburger').addEventListener('click', function (event
     if (this.classList.contains('is-active')) {
         this.classList.remove('is-active');
         document.querySelector('.nav-list').classList.remove('nav-active');
+        $(document.querySelector('.nav-active')).fadeOut()
     }
     else {
         this.classList.add('is-active');
         document.querySelector('.nav-list').classList.add('nav-active');
+        $(document.querySelector('.nav-active')).fadeIn()
     }
 })
 
-
+document.querySelector('.nav-list').addEventListener('click', () => {
+    document.querySelector('.c-hamburger').classList.remove('is-active');
+    $(document.querySelector('.nav-active')).fadeOut()
+})
